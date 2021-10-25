1c0e61ab7cc19942854cda4ba2f42990338c3ffc
Iurii <velheorr@gmail.com>
2021-10-25 15:32:04 +0500

    97

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,21 +5,29 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+    const data = [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
         </div>
-        
-        <EmployeesList/>
-        <EmployeesAddForm/>
-    </div>
-  );
+    );
 }
 
 export default App;
